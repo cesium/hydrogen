ead16d5204a4e16b84466621f510718c94f09580
Gerson Junior <58490266+GresosJ@users.noreply.github.com>
2023-07-25 15:43:04 +0100

    Navbar (#3)
    * navbar
    
    * run format
    
    * Fix details
    
    * run format

---
+++ components/Navbar.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+import logo from "@/public/cesium.png";
+import Link from "next/link";
+
+const navbar: string[] = ["HOME", "TEAM", "LOGS", "EVENTS", "ABOUT US"];
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between px-16">
+      <Image src={logo} alt="" width={150} height={200} priority unoptimized />
+      <div className="flex w-[50%] items-center justify-between">
+        {navbar.map((title) => (
+          <Link
+            key={title}
+            className="border-b-2 border-transparent pb-4 text-sm text-black hover:border-cesium-orange hover:text-cesium-orange"
+            href="/"
+          >
+            {title}
+          </Link>
+        ))}
+      </div>
+      <div className="h-20 w-20 rounded-full bg-gray-200"></div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,40 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.5rem" }],
+      base: ["1rem", { lineHeight: "1.5rem" }],
+      lg: ["1.125rem", { lineHeight: "2rem" }],
+      xl: ["1.25rem", { lineHeight: "2rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2.5rem" }],
+      "3xl": ["2rem", { lineHeight: "2.5rem" }],
+      "4xl": ["2.5rem", { lineHeight: "3rem" }],
+      "5xl": ["3rem", { lineHeight: "3.5rem" }],
+      "6xl": ["4rem", { lineHeight: "1" }],
+      "7xl": ["5rem", { lineHeight: "1" }],
+      "8xl": ["6rem", { lineHeight: "1" }],
+      "9xl": ["8rem", { lineHeight: "1" }],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "cesium-orange": {
+          DEFAULT: "#ED8661",
+          100: "#FFEDE7",
+          200: "#FBE4DC",
+          300: "#F9D7CA",
+          400: "#F7C9B9",
+          500: "#F5BCA7",
+          600: "#F3AE95",
+          700: "#F1A184",
+          800: "#ED8661",
+          900: "#ED8661",
+        },
       },
     },
   },
