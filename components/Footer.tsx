690efa8eb16a5cfe3b7e2b022d279b127b07cbc8
Afonso Pimenta <pimenta.afonsosilva@gmail.com>
2023-07-28 03:00:38 +0100

    complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ const ListLink = ({ href, text }: ListLinkProps) => (
 const Footer = () => {
   return (
     <footer className="mt-12 grid gap-4 px-4 pb-4 md:grid-cols-2 md:px-8 md:pb-8 lg:grid-cols-3 lg:px-20 lg:pb-24">
-      <div className="flex flex-col justify-between text-gray-500 max-lg:gap-4">
+      <div className="flex flex-col justify-between text-gray-500 max-lg:gap-4 max-md:items-center max-md:text-center">
         <Image
           src="/cesium.svg"
           alt="CeSIUM logo"
@@ -36,7 +36,7 @@ const Footer = () => {
           CeSIUM - Centro de Estudantes de Engenharia Informática da
           Universidade do Minho
         </p>
-        <ul className="flex gap-2">
+        <ul className="flex flex-wrap gap-2">
           <SocialLink href="" icon="Facebook" />
           <SocialLink href="" icon="Instagram" />
           <SocialLink href="" icon="Twitter" />
@@ -44,14 +44,14 @@ const Footer = () => {
           <SocialLink href="" icon="Youtube" />
         </ul>
       </div>
-      <div className="max-lg:border-t-[1px] max-lg:border-gray-300 max-lg:pt-4 md:col-span-2 lg:flex lg:justify-center lg:col-span-1">
-        <div className="grid grid-cols-3 gap-4 max-w-md">
+      <div className="flex justify-center max-lg:pt-4 max-md:border-t-[1px] max-md:border-gray-300">
+        <div className="grid max-w-md grow grid-cols-3 gap-4">
           <div>
             <span>Label</span>
             <ul className="mt-4">
-              <ListLink href="" text="consectetur" />
+              <ListLink href="" text="Consectetur" />
               <ListLink href="" text="Lorem Ipsum" />
-              <ListLink href="" text="Lorem Ipsum" />
+              <ListLink href="" text="Lorem" />
             </ul>
           </div>
           <div>
@@ -72,12 +72,14 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="text-gray-500 max-md:border-t-[1px] max-md:border-gray-300 max-md:pt-4 md:col-start-2 md:row-start-1 md:self-end md:text-right lg:col-start-auto lg:row-start-auto">
-        <p>Braga, Portugal</p>
-        <p>Telefone: +351 253 604 448</p>
-        <p>
-          Email: <a href="emailto:cesium@di.uminho.pt">cesium@di.uminho.pt</a>
-        </p>
+      <div className="text-center text-gray-500 max-lg:flex max-lg:justify-center max-lg:border-t-[1px] max-lg:border-gray-300 max-lg:pt-4 max-md:pt-4 md:col-span-2 md:self-end lg:col-auto lg:text-right">
+        <div>
+          <p>Braga, Portugal</p>
+          <p>Telefone: +351 253 604 448</p>
+          <p>
+            Email: <a href="mailto:cesium@di.uminho.pt">cesium@di.uminho.pt</a>
+          </p>
+        </div>
       </div>
     </footer>
   );
