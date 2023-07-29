3211c63acf08811ce00923fa371c2dabc6e9d8ef
Afonso Pimenta <pimenta.afonsosilva@gmail.com>
2023-07-29 20:00:48 +0100

    Add footer (#6)
    * checkpoint
    
    * complete
    
    * fix spacings
    
    * run formatter
    
    * add social icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,98 @@
+import type { ReactNode } from "react";
+import Image from "next/image";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaGithub,
+  FaYoutube,
+} from "react-icons/fa";
+
+type SocialLinkProps = {
+  href: string;
+  icon: ReactNode;
+};
+
+const SocialLink = ({ href, icon }: SocialLinkProps) => (
+  <li>
+    <a href={href} className="[&>svg]:h-6 [&>svg]:w-6">
+      {icon}
+    </a>
+  </li>
+);
+
+type ListLinkProps = {
+  href: string;
+  text: string;
+};
+
+const ListLink = ({ href, text }: ListLinkProps) => (
+  <li className="py-1 text-gray-500">
+    <a href={href}>{text}</a>
+  </li>
+);
+
+const Footer = () => {
+  return (
+    <footer className="mt-12 grid gap-4 px-4 pb-4 md:grid-cols-2 md:px-8 md:pb-8 lg:grid-cols-3 lg:px-20 lg:pb-24">
+      <div className="flex flex-col justify-between gap-4 text-gray-500 max-md:items-center max-md:text-center">
+        <Image
+          src="/cesium.svg"
+          alt="CeSIUM logo"
+          width={32}
+          height={32}
+        ></Image>
+        <p className="max-w-[40ch]">
+          CeSIUM - Centro de Estudantes de Engenharia Informática da
+          Universidade do Minho
+        </p>
+        <ul className="flex flex-wrap gap-x-4">
+          <SocialLink href="" icon={<FaFacebookF />} />
+          <SocialLink href="" icon={<FaInstagram />} />
+          <SocialLink href="" icon={<FaTwitter />} />
+          <SocialLink href="" icon={<FaGithub />} />
+          <SocialLink href="" icon={<FaYoutube />} />
+        </ul>
+      </div>
+      <div className="flex justify-center max-lg:pt-4 max-md:border-t-[1px] max-md:border-gray-300">
+        <div className="grid max-w-md grow grid-cols-3 gap-4">
+          <div>
+            <span>Label</span>
+            <ul className="mt-4">
+              <ListLink href="" text="Consectetur" />
+              <ListLink href="" text="Lorem Ipsum" />
+              <ListLink href="" text="Lorem" />
+            </ul>
+          </div>
+          <div>
+            <span>Label</span>
+            <ul className="mt-4">
+              <ListLink href="" text="Lorem Ipsum" />
+              <ListLink href="" text="Dolor" />
+              <ListLink href="" text="Sit Amet" />
+            </ul>
+          </div>
+          <div>
+            <span>Label</span>
+            <ul className="mt-4">
+              <ListLink href="" text="Facilis nam" />
+              <ListLink href="" text="Itaque eius" />
+              <ListLink href="" text="Lorem Ipsum" />
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="text-center text-gray-500 max-lg:flex max-lg:justify-center max-lg:border-t-[1px] max-lg:border-gray-300 max-lg:pt-4 max-md:pt-4 md:col-span-2 md:self-end lg:col-auto lg:text-right">
+        <div>
+          <p>Braga, Portugal</p>
+          <p>Telefone: +351 253 604 448</p>
+          <p>
+            Email: <a href="mailto:cesium@di.uminho.pt">cesium@di.uminho.pt</a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
