25e1aca27bebf79348828e7a0d90b19e020c3f88
Gerson Junior <58490266+GresosJ@users.noreply.github.com>
2023-07-31 17:43:30 +0100

    Banners (#5)
    * Hero Section
    
    * Fix minor error
    
    * Fix banners
    
    * Add hover
    
    * Redo handleColSpan
    
    * Add LinkGroup type

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Hero from "@/components/Hero";
+import Banners from "@/components/Banners";
 import PartnerBenefits from "@/components/PartnerBenefits";
 
 export default function Home() {
   return (
     <main className="bg-background">
       <Hero />
-
+      <Banners />
       <PartnerBenefits />
     </main>
   );
---
+++ components/Banners.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import React from "react";
+
+type LinkGroup = string[];
+const items: LinkGroup[] = [
+  ["Ser Parceiro", "Parceiros", "Equipa"],
+  ["Sobre Nós"],
+  ["Departamento de Informática", "Estudar LEI"],
+];
+
+interface RowProps {
+  links: LinkGroup;
+  rowIndex: number;
+}
+
+function handleColSpan(rowIndex: number, colIndex: number): string {
+  if (rowIndex === 0) {
+    return colIndex === 0 ? "col-span-2" : "col-span-1";
+  } else if (rowIndex === 1) {
+    return "col-span-4";
+  } else if (rowIndex === 2) {
+    return "col-span-2";
+  } else {
+    return "col-span-1";
+  }
+}
+
+function Row({ links, rowIndex }: RowProps) {
+  return (
+    <React.Fragment>
+      {links.map((item: string, colIndex: number) => (
+        <Link
+          href="/"
+          key={colIndex}
+          className={`relative ${handleColSpan(
+            rowIndex,
+            colIndex,
+          )} h-[250px] rounded-xl bg-gray-200 text-2xl font-bold text-white hover:bg-gray-300`}
+        >
+          <span className="absolute bottom-4 left-14">{item}</span>
+        </Link>
+      ))}
+    </React.Fragment>
+  );
+}
+
+export default function Banners() {
+  const rows: JSX.Element[] = items.map((row: string[], rowIndex: number) => (
+    <Row links={row} rowIndex={rowIndex} key={rowIndex} />
+  ));
+
+  return (
+    <div className="mx-auto my-32 grid w-[1280px] grid-cols-4 gap-4 2xl:mt-60">
+      {rows}
+    </div>
+  );
+}
