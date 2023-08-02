11b115c9a0c8d639f8596cac86842adff43e5b58
Sushe <jlopesum004@gmail.com>
2023-08-02 21:33:45 +0100

    logs as a list with keys

--- app/logs/page.tsx
+++ app/logs/page.tsx
@@ -2,6 +2,8 @@ import LogBanner from "@/components/LogBanner";
 import logs from "@/data/logs.json";
 
 export default function Logs() {
+  const logsWithId = logs.map((article, index) => ({ ...article, id: index }));
+
   return (
     <main className="bg-background">
       <h1 className="flex justify-center text-4xl font-semibold text-[#353335]">
@@ -9,16 +11,13 @@ export default function Logs() {
       </h1>
 
       <div className="flex justify-center">
-        <div className="pt-12">
-          {logs.map((article) => (
-            <LogBanner
-              title={article.title}
-              content={article.content}
-              timePublished={article.timePublished}
-              path={article.path}
-            />
+        <ul className="pt-12">
+          {logsWithId.map((article) => (
+            <li key={article.id}>
+              <LogBanner {...article} />
+            </li>
           ))}
-        </div>
+        </ul>
       </div>
     </main>
   );
