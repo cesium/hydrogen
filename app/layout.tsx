41382658e62a24df1c43046707d2e6a4c98c7f38
Diogo Matos <diogomatosemail@gmail.com>
2023-09-28 22:49:59 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,12 +54,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [width, setWidth] = useState<number>(0);
-  const [height, setHeight] = useState<number>(0);
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [height, setHeight] = useState<number>(window.innerHeight);
 
+  // "Listens" for changes in the window size and updates the nr of lines accordingly
   useEffect(() => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
