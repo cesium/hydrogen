4d512b77bf646aefa6713ac62b426a734651ffe0
11GG20 <fl11gg20@gmail.com>
2024-11-26 00:33:04 +0000

    feat: link component

---
+++ src/components/link.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link"
+
+interface ILink {
+    title: string,
+    href: string,
+    arrow: 'back'| 'forward'| 'outward'
+}
+
+const LinkProps = ({title, href, arrow}: ILink) => {
+    switch(arrow) {
+        case 'back':
+            return (
+                <Link href={{href}} className="flex items-center text-primary gap-1 font-medium">
+                    <span className="material-symbols-outlined">arrow_{arrow}</span>
+                    <p>{title}</p>
+                </Link>
+              )
+        case 'forward': 
+        case 'outward':
+            return (
+                <Link href={{href}} className="flex items-center text-primary gap-1 font-medium">
+                    <p>{title}</p>
+                    <span className="material-symbols-outlined">arrow_{arrow}</span>
+                </Link>
+              )
+    }
+}
+
+export default LinkProps
