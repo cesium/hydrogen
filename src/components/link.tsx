8391e6fc53c0ce4ef28bb38d93a1de8b9129bfaa
11GG20 <fl11gg20@gmail.com>
2024-11-26 01:21:37 +0000

    refactor: link component conditions

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,29 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
 
 interface ILink {
-    title: string,
-    href: string,
-    arrow: 'back'| 'forward'| 'outward'
+  title: string;
+  href: string;
+  arrow: "back" | "forward" | "outward";
 }
 
-const CostumLink = ({title, href, arrow}: ILink) => {
-    switch(arrow) {
-        case 'back':
-            return (
-                <Link href={{href}} className="flex items-center text-primary gap-1 font-medium">
-                    <span className="material-symbols-outlined">arrow_{arrow}</span>
-                    <p>{title}</p>
-                </Link>
-              )
-        case 'forward': 
-        case 'outward':
-            return (
-                <Link href={{href}} className="flex items-center text-primary gap-1 font-medium">
-                    <p>{title}</p>
-                    <span className="material-symbols-outlined">arrow_{arrow}</span>
-                </Link>
-              )
-    }
-}
+const CustomLink = ({ title, href, arrow }: ILink) => {
+  return (
+    <Link
+      href={{ href }}
+      className="flex items-center gap-1 font-medium text-primary"
+    >
+      {arrow === "back" && (
+        <span className="material-symbols-outlined">arrow_{arrow}</span>
+      )}
+      <p>{title}</p>
+      {(arrow === "forward" || arrow === "outward") && (
+        <span className="material-symbols-outlined">arrow_{arrow}</span>
+      )}
+    </Link>
+  );
+};
 
-export default CostumLink
+export default CustomLink;
