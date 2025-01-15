a7ae2b3effc9d2c4d4cf4348acdad8931a14a433
AfonsoMartins26 <afonsopm2005@gmail.com>
2025-01-15 21:37:15 +0000

    fix: one error on the build of the app

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,6 +3,7 @@ import DepartmentCard from "@/components/department-card";
 import PromotionalCard from "@/components/promotional-card";
 import Carousel from "@/components/carousel";
 import { CardType } from "@/lib/types";
+import Image from "next/image";
 
 export default function Home() {
   const images = [
@@ -15,18 +16,21 @@ export default function Home() {
   ];
 
   const text = [
-    <div className="h-[270px] w-[343px] bg-gradient-to-r from-black/50 via-black/25 to-black/50 bg-clip-text pl-6 font-title text-[36px] font-medium leading-[125%] text-transparent md:h-[360px] md:w-[565px] md:text-[48px]">
+    <div
+     key="text-1"
+     className="h-[270px] w-[343px] bg-gradient-to-r from-black/50 via-black/25 to-black/50 bg-clip-text pl-6 font-title text-[36px] font-medium leading-[125%] text-transparent md:h-[360px] md:w-[565px] md:text-[48px]">
       <p>
         Centro de
         <br /> Estudantes de
-        <p className="font-title text-[36px] text-black md:text-[48px]">
-          {" "}
-          Engenharia Informática
-        </p>
+        <span className="font-title text-[36px] text-black md:text-[48px]">
+          {" "}Engenharia Informática
+        </span>{" "}
         da Universidade do Minho
       </p>
     </div>,
-    <div className=" h-[288px] w-[343px] md:h-[168px] md:w-[636px]">
+    <div
+    key="text-2" 
+    className="h-[288px] w-[343px] md:h-[168px] md:w-[636px]">
       <div className="text-justify font-sans text-[12px] font-normal leading-[24px] text-[#6E6E6E] md:text-[16px]">
         <p>
           Decorria no ano de 1995, a Licenciatura em Engenharia de Sistemas e
@@ -36,7 +40,7 @@ export default function Home() {
           desse momento iniciou-se o caminho deste núcleo estudantil que, 20
           anos depois, mantém o nome e sigla e mantém o propósito determinado na
           projeção da imagem do curso e na defesa dos interesses dos seus
-          associados
+          associados.
         </p>
       </div>
       <br />
@@ -46,6 +50,7 @@ export default function Home() {
       </div>
     </div>,
   ];
+  
 
   return (
     <main className="flex-col items-center justify-center">
@@ -63,10 +68,10 @@ export default function Home() {
         <PromotionalCard type={CardType.Membership} />
       </div>
       <div className="w-full pt-6">
-        <Carousel items={images.map((image) => ({ image }))} />
+      <Carousel items={images.map((image, index) => ({ image, key: `image-${index}` }))} />
       </div>
       <div className="flex justify-center pt-6">
-        <Carousel items={text.map((t) => ({ text: t }))} />
+      <Carousel items={text.map((t, index) => ({ text: t, key: `text-${index}` }))} />
       </div>
     </main>
   );
