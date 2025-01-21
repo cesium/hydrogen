da3cbff8e7bb082cfc5eb74b94251e7c51d024eb
11GG20 <fl11gg20@gmail.com>
2025-01-21 16:38:18 +0000

    feat: about section layout

---
+++ src/components/aboutsection.tsx
@@ -0,0 +1,33 @@
+import CustomLink from "./link";
+
+interface AboutSectionProps {
+  title: string;
+  subtitle: string;
+  children: React.ReactNode;
+}
+
+const AboutsectionLayout = ({ title, subtitle, children }: AboutSectionProps) => {
+  return (
+    <div className="flex flex-col items-stretch px-6 pt-10 sm:flex-row sm:p-12">
+      <div className="flex w-full mb-4 sm:mr-6 sm:w-20">
+        <div className="flex h-fit items-start justify-center sm:h-full sm:w-full">
+          <span className="w-fit origin-right font-title text-2xl font-medium sm:translate-x-[-50%] sm:translate-y-[-50%] sm:-rotate-90 sm:pr-1 sm:text-3xl">
+            {title}
+          </span>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex justify-start">{subtitle}</div>
+
+        <div className="sm:my-10 my-7">{children}</div>
+
+        <div className="hidden sm:block">
+          <CustomLink title="Ver mais" href="#" arrow="forward" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutsectionLayout;
