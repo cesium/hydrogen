435ef77ff37e79c6d435f5682a20a7838b5fb582
Diogo Matos <diogomatosemail@gmail.com>
2025-01-24 19:14:12 +0000

    feat: `/about/departments` page (#59)

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,26 +1,35 @@
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useLang } from "@/contexts/dictionary-provider";
 
 interface LinkProps {
   title: string;
-  href: string;
+  href?: string;
   arrow?: "back" | "forward" | "outward";
 }
 
-const CustomLink = ({ title, href, arrow }: LinkProps) => {
-  return (
-    <Link
-      href={href}
-      className="flex items-center gap-1 font-medium text-primary transition-opacity hover:opacity-85"
-    >
-      {arrow === "back" && (
-        <span className="material-symbols-outlined">arrow_{arrow}</span>
-      )}
+const AppLink = ({ title, href, arrow }: LinkProps) => {
+  const lang = useLang();
+  const hrefDefault = href ?? "/";
+  const hrefLang = `/${lang}${href}`;
+  const router = useRouter();
+
+  const style =
+    "flex items-center gap-1 font-medium text-primary transition-opacity hover:opacity-85";
+
+  return arrow === "back" ? (
+    <button onClick={() => router.back()} className={style}>
+      <span className="material-symbols-outlined">{"arrow_" + arrow}</span>
+      <p>{title}</p>
+    </button>
+  ) : (
+    <Link href={arrow === "forward" ? hrefLang : hrefDefault} className={style}>
       <p>{title}</p>
       {(arrow === "forward" || arrow === "outward") && (
-        <span className="material-symbols-outlined">arrow_{arrow}</span>
+        <span className="material-symbols-outlined">{"arrow_" + arrow}</span>
       )}
     </Link>
   );
 };
 
-export default CustomLink;
+export default AppLink;
