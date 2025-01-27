1a9038032a836ae97a8158f692df2a6790c98080
RicoPleasure <esprazj.dev@gmail.com>
2025-01-27 23:19:36 +0000

    chore: proposed changes on review

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -17,8 +17,6 @@ const ProjectCard = ({ type }: ProjectProps) => {
         return {
           src: "/logo/sei.svg",
           alt: "SEI Logo",
-          width: "57",
-          height: "38",
           gradient_color: "rgba(35,11,183,0.10)",
           ref: "https://seium.org/",
         };
@@ -26,8 +24,6 @@ const ProjectCard = ({ type }: ProjectProps) => {
         return {
           src: "/logo/bugsbyte.svg",
           alt: "BugsByte Logo",
-          width: "220",
-          height: "36",
           gradient_color: "rgba(120,176,69,0.25)",
           ref: "https://bugsbyte.org/",
         };
@@ -35,8 +31,6 @@ const ProjectCard = ({ type }: ProjectProps) => {
         return {
           src: "/logo/coderdojo.svg",
           alt: "CoderDojo Logo",
-          width: "162",
-          height: "47",
           gradient_color: "rgba(50,51,51,0.25)",
           ref: "https://coderdojobraga.org/",
         };
@@ -44,8 +38,6 @@ const ProjectCard = ({ type }: ProjectProps) => {
         return {
           src: "/logo/sei.svg",
           alt: "SEI Logo",
-          width: "57",
-          height: "38",
           gradient_color: "rgba(35,11,183,0.10)",
           ref: "https://seium.org/",
         };
@@ -72,17 +64,19 @@ const ProjectCard = ({ type }: ProjectProps) => {
 
   return (
     <div
-      className={`mt-[30px] content-center border-b border-[#230BB71A] pb-[30px] md:mt-0 md:min-w-[460px] lg:w-1/3 lg:border-none`}
+      className={`mt-[30px] border-b border-[#230BB71A] pb-[30px] md:mt-0 md:min-w-[460px] lg:w-1/3 lg:border-none`}
       style={{
         background: `radial-gradient(circle at center 130%, ${info().gradient_color ?? "rgba(50,51,51,0.25)"} 10%, rgba(50,51,51,0) 57%)`,
       }}
     >
-      <Image
-        src={info().src}
-        alt={info().alt}
-        width={Number(info().width) ?? 100}
-        height={Number(info().height) ?? 40}
-      />
+      <div className="relative w-auto h-[50px] flex">
+          <Image
+            src={info().src}
+            alt={info().alt}
+            fill
+            className="object-contain object-left-top"
+          />
+      </div>
       <p className="pt-4 md:hidden md:min-w-[460px]">
         {project.mobile_description}
       </p>
