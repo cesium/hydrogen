e3cb8468132171271f8b32d9fa76747b9d37bc47
RicoPleasure <esprazj.dev@gmail.com>
2025-02-18 10:49:55 +0000

    feat: add cards into projects layout

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import AboutSectionLayout from "@/components/about-section-layout";
 import ProjectCard from "@/components/project-card";
+import { useDictionary } from "@/contexts/dictionary-provider";
 
 const About = () => {
+  const dict = useDictionary()
   return (
     <>
       <div className="flex flex-col">
-        <div
-          className="flex flex-col md:flex-row md:gap-4 md:overflow-auto"
-          style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
-        >
-        </div>
+        <AboutSectionLayout title={dict.about.projects.title} subtitle={dict.about.projects.description} href="#">
+          <div
+              className="flex flex-col md:flex-row md:gap-4 md:overflow-auto"
+              style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+            >
+              <ProjectCard type="sei"/>
+              <ProjectCard type="bugsbyte"/>
+              <ProjectCard type="coderdojo"/>
+            </div>
+        </AboutSectionLayout>
       </div>
     </>
   );
