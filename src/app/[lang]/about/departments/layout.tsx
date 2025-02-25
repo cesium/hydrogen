ac5eaf50e12e0b9596776422119cfd00251a156d
Diogo Matos <me@diogogmatos.dev>
2025-02-25 00:13:37 +0000

    fix: move metadata to layout

---
+++ src/app/[lang]/about/departments/layout.tsx
@@ -0,0 +1,56 @@
+import {
+  getDictionary,
+  type Locale,
+} from "@/internationalization/dictionaries";
+import { type Metadata } from "next";
+
+export function generateMetadata({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}): Metadata {
+  const dict = getDictionary(lang);
+
+  return {
+    title: dict.seo.departments.title,
+    description: dict.seo.departments.description,
+    keywords: [
+      "student center",
+      "engeneering",
+      "informatics",
+      "uminho",
+      "university",
+      "students",
+      "CeSIUM",
+      "CeSIUM UMinho",
+      "departments",
+    ],
+    openGraph: {
+      url: "https://cesium.di.uminho.pt/about/departments",
+      type: "website",
+      title: dict.seo.departments.title,
+      description: dict.seo.departments.description,
+      images: [
+        {
+          url: "https://cesium.di.uminho.pt/og.png",
+          width: 1200,
+          height: 630,
+          alt: "cesium.di.uminho.pt",
+        },
+      ],
+    },
+    alternates: {
+      canonical: "https://cesium.di.uminho.pt/about/departments",
+      languages: {
+        en: "https://cesium.di.uminho.pt/en_US/about/departments",
+        pt: "https://cesium.di.uminho.pt/pt_PT/about/departments",
+      },
+    },
+  };
+}
+
+export default function DepartmentsLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return <>{children}</>;
+}
--- src/app/[lang]/about/departments/page.tsx
+++ src/app/[lang]/about/departments/page.tsx
@@ -9,56 +9,6 @@ import { CardType, type TeamData } from "@/lib/types";
 import { fetchTeamData } from "@/lib/utils";
 import { useState, useEffect } from "react";
 
-import {
-  getDictionary,
-  type Locale,
-} from "@/internationalization/dictionaries";
-import { type Metadata } from "next";
-
-export function generateMetadata({
-  params: { lang },
-}: {
-  params: { lang: Locale };
-}): Metadata {
-  const dict = getDictionary(lang);
-
-  return {
-    title: dict.seo.departments.title,
-    description: dict.seo.departments.description,
-    keywords: [
-      "student center",
-      "engeneering",
-      "informatics",
-      "uminho",
-      "university",
-      "students",
-      "CeSIUM",
-      "CeSIUM UMinho",
-    ],
-    openGraph: {
-      url: "https://cesium.di.uminho.pt/about/departments",
-      type: "website",
-      title: dict.seo.departments.title,
-      description: dict.seo.departments.description,
-      images: [
-        {
-          url: "https://cesium.di.uminho.pt/og.png",
-          width: 1200,
-          height: 630,
-          alt: "cesium.di.uminho.pt",
-        },
-      ],
-    },
-    alternates: {
-      canonical: "https://cesium.di.uminho.pt/about/departments",
-      languages: {
-        en: "https://cesium.di.uminho.pt/en_US/about/departments",
-        pt: "https://cesium.di.uminho.pt/pt_PT/about/departments",
-      },
-    },
-  };
-}
-
 export default function Departments() {
   const dict = useDictionary();
 
