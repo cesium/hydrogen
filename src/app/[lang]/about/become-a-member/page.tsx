fe35c9576651684a1d883ed04e83ffc1986c1fe5
Júlio Pinto <pinto.julioj03@gmail.com>
2025-02-27 18:29:45 +0000

    refactor: remove useless classes

--- src/app/[lang]/about/become-a-member/page.tsx
+++ src/app/[lang]/about/become-a-member/page.tsx
@@ -96,38 +96,33 @@ export default function BecomeAMember() {
       <InfoCard>
         <div className="flex flex-col gap-4">
           <div className="grid h-full sm:h-96 grid-cols-2 sm:grid-cols-3">
-            <div className="relative order-2 w-full h-full max-w-[480px] justify-self-start -ml-5 mb-12 sm:-ml-12 sm:-mt-24 lg:order-1 lg:self-start">
+            <div className="relative order-2 w-full h-full max-w-[480px] justify-self-start -mb-4 sm:mb-0 -ml-4 sm:-ml-12 sm:-mt-24 lg:order-1">
               <Image
                 src="/images/notebook.png"
                 alt="discounts"
                 layout="fill"
                 objectFit="cover"
-                className="object-[0%_0%] sm:object-[100%_100%]" // Default: Top-left on small, Bottom-right on large
+                className="object-[0%_0%] sm:object-[100%_100%]"
               />
             </div>
 
             <div className="order-1 col-span-2 flex items-center justify-center h-full sm:order-2 sm:col-span-1">
               <div className="flex flex-col items-center gap-2 px-12 py-10 sm:px-0">
-                <div className="flex flex-col items-center gap-2 mx-auto lg:flex-row">
-                  <span className="text-4xl text-[#5C657F] material-symbols-outlined">
-                    percent
-                  </span>
+                <div className="flex flex-col items-center gap-2 lg:flex-row">
+                  <span className="text-4xl text-[#5C657F] material-symbols-outlined">percent</span>
                   <h2 className="text-2xl font-semibold text-center font-title">
                     {dict.about.become_a_member.discounts.title}
                   </h2>
                 </div>
                 <p className="text-center">{dict.about.become_a_member.discounts.description}</p>
-                <a
-                  className="flex flex-row items-center justify-center gap-1 mx-auto text-[#5C657F]"
-                  href="https://store.cesium.pt"
-                >
+                <a className="flex items-center gap-1 text-[#5C657F]" href="https://store.cesium.pt">
                   <p className="text-center">{dict.about.become_a_member.discounts.link}</p>
                   <span className="leading-8 material-symbols-outlined">north_east</span>
                 </a>
               </div>
             </div>
 
-            <div className="relative order-3 w-full h-full max-w-[480px] justify-self-end -mr-5 pb-32 sm:pb-0 -mb-4 sm:-mr-12 sm:mt-24">
+            <div className="relative order-3 w-full h-full max-w-[480px] justify-self-end pb-32 sm:pb-0 -mb-4 sm:mb-0 -mr-4 sm:-mr-12 sm:mt-24">
               <Image
                 src="/images/laptop.png"
                 alt="discounts"
@@ -139,8 +134,6 @@ export default function BecomeAMember() {
           </div>
         </div>
       </InfoCard>
-
-
       </div>
     </main>
   );
