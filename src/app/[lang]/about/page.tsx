fbbfe5b39ba4408d316ae2d9bbb1d45f8511d54c
Enrico Prazeres <esprazj.dev@gmail.com>
2025-02-27 16:39:19 +0000

    feat: `/about` page Projects section  (#103)
    * feat: add cards into projects layout
    
    * fix: scroll card components
    
    * fix: conflicts
    
    * chore: add href to see more button
    
    * fix: export default function

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,7 +1,30 @@
 "use client";
+import AboutSectionLayout from "@/components/about-section-layout";
+import ProjectCard from "@/components/project-card";
+import { useDictionary } from "@/contexts/dictionary-provider";
 
 export default function About() {
+  const dict = useDictionary();
   return (
-    <></>
+    <>
+      <div className="flex flex-col">
+        <AboutSectionLayout
+          linkName="see_more"
+          titleOrientation="vertical"
+          title={dict.about.projects.title}
+          subtitle={dict.about.projects.description}
+          href="/projects"
+        >
+          <div
+            className="flex flex-col md:flex-row md:gap-4"
+            style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+          >
+            <ProjectCard type="sei" />
+            <ProjectCard type="bugsbyte" />
+            <ProjectCard type="coderdojo" />
+          </div>
+        </AboutSectionLayout>
+      </div>
+    </>
   );
 }
