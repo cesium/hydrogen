2a6405b85a3d5057981a468a01b5b069729140bb
11GG20 <fl11gg20@gmail.com>
2025-02-27 17:14:43 +0000

    refactor: use new carousel & improve section

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -3,45 +3,41 @@
 import { useDictionary } from "@/contexts/dictionary-provider";
 import Carousel from "@/components/carousel";
 import Image from "next/image";
+import { horizontalPadding } from "@/lib/styling";
 
 export default function About() {
 
   const dict = useDictionary();
   const dictAbout = dict.about;
 
-  const images = [
-    "/images/uMinho1.jpg",
-    "/images/sei24-25.jpg",
-    "/images/uMinho2.jpg",
-    "/images/uMinho1.jpg",
-    "/images/sei24-25.jpg",
-    "/images/uMinho2.jpg",
-  ];
+  const images = dictAbout.sections.cesium.images;
   
   return (
-    <>
-
-      <section className={`flex flex-col text-center pt-24 pb-12 gap-4 sm:gap-6 border-b-[1px] border-[#0000001A]`}>
-        <p className="font-title font-medium text-2xl sm:text-3xl">{dictAbout.sections[0]?.title}</p>
-        <p className="px-6 sm:px-12">{dictAbout.sections[0]?.subtitle}</p>
-        <div className="w-full pt-6">
+    <main>
+      <section className={`flex flex-col items-center text-center py-12 gap-4 sm:gap-6 border-b-[1px] border-[#0000001A] bg-gray/`}>
+        <p className="font-title font-medium text-2xl sm:text-3xl">{dictAbout.sections.cesium.title}</p>
+        <p className="px-6 sm:px-64">{dictAbout.sections.cesium.subtitle}</p>
+        <div className="w-full overflow-hidden pt-4 sm:pt-6">
           <Carousel
-            autoplay={3000}
+            autoplay={2000}
+            pagination
+            overflow
+            loop
             items={images.map((image, index) => (
-              <Image
-                key={index}
-                src={image}
-                width={400}
-                height={400}
-                alt=""
-                className="h-[400px] w-full rounded-xl object-cover"
-              />
+              <div key={index} className="flex justify-center">
+                <Image
+                  src={image.src}
+                  width={490}
+                  height={367}
+                  alt={image.alt}
+                  className="h-[237px] w-full rounded-xl object-cover sm:h-[367px] sm:max-w-full"
+                />
+              </div>
             ))}
           />
         </div>
-        <p className="px-6 sm:px-12">{dictAbout.sections[0]?.description}</p>
+        <p className="px-6 sm:px-64">{dictAbout.sections.cesium.description}</p>
       </section>
-
-    </>
+    </main>
   );
 };
