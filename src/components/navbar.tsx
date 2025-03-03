0b34301b58ba407fc834da102c040ffc2864b587
Diogo Matos <diogomatosemail@gmail.com>
2025-03-03 16:01:48 +0000

    feat: layout improvements (#111)
    * feat: layout improvements
    
    * fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 import SocialIcon from "@/components/social-icon";
 import Logo from "./logo";
+import { horizontalPadding } from "@/lib/styling";
 
 const Navbar = () => {
   const dict = useDictionary();
@@ -52,127 +53,129 @@ const Navbar = () => {
       : "text-gray";
 
   return (
-    <div
-      className={`${navbarBackgroundColor} ${isMemberOrCollaborator ? "relative after:hidden" : "sticky"} top-0 z-40 flex w-full flex-col px-5 pb-3 pt-4 after:absolute after:bottom-0 after:left-0 after:h-6 after:w-full after:translate-y-6 after:bg-gradient-to-b after:from-background after:to-transparent md:relative md:px-20 md:pt-12 after:md:hidden`}
-    >
-      <nav className="flex items-center justify-between gap-9 md:justify-normal">
-        <Link href="/">
-          <Logo
-            type="cesium"
-            width={30}
-            height={34}
-            alt="CeSIUM Logo Icon"
-            fill={`${colorLogo}`}
-            className="hidden md:block"
-            fullColor={isMemberOrCollaborator}
-          />
-          <Logo
-            type="cesium-full"
-            width={103}
-            height={32}
-            alt="CeSIUM Logo"
-            fill={`${colorLogo}`}
-            className="block md:hidden"
-            fullColor={isMemberOrCollaborator}
-          />
-        </Link>
-        <div
-          className={`hidden items-center space-x-6 font-title text-lg font-medium ${linkColor} md:flex`}
-        >
-          {routes.map((route) => (
-            <Link
-              key={route.path}
-              href={route.path}
-              className={`${
-                isCurrent(route.path) ? `${currentLink}` : ""
-              } transition-colors ${isMemberOrCollaborator ? "hover:text-white" : "hover:text-black"}`}
-            >
-              {route.name}
-            </Link>
-          ))}
-        </div>
-        <motion.button
-          className={`${hamburgerMenuColor} material-symbols-outlined z-50 p-1 text-3xl md:hidden`}
-          onClick={() => setIsOpen(!isOpen)}
-          animate={{ rotate: isOpen ? 90 : 0 }}
-        >
-          {isOpen ? "close" : "menu"}
-        </motion.button>
-      </nav>
-      <AnimatePresence>
-        {isOpen && (
-          <motion.nav
-            className="fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col justify-between bg-[#F0F0F0] px-7 pb-8 pt-20 shadow-lg"
-            exit={{ x: "100%", borderRadius: "50px" }}
-            initial={{ scale: 1.15 }}
-            animate={{ scale: 1 }}
-            transition={{ duration: 0.15 }}
+    <div className={horizontalPadding}>
+      <div
+        className={`${navbarBackgroundColor} ${isMemberOrCollaborator ? "relative after:hidden" : "sticky"} top-0 z-40 flex w-full flex-col pb-3 pt-4 after:absolute after:bottom-0 after:left-0 after:h-6 after:w-full after:translate-y-6 after:bg-gradient-to-b after:from-background after:to-transparent md:relative md:px-12 md:pt-12 after:md:hidden`}
+      >
+        <nav className="flex items-center justify-between gap-9 md:justify-normal">
+          <Link href="/">
+            <Logo
+              type="cesium"
+              width={30}
+              height={34}
+              alt="CeSIUM Logo Icon"
+              fill={`${colorLogo}`}
+              className="hidden md:block"
+              fullColor={isMemberOrCollaborator}
+            />
+            <Logo
+              type="cesium-full"
+              width={103}
+              height={32}
+              alt="CeSIUM Logo"
+              fill={`${colorLogo}`}
+              className="block md:hidden"
+              fullColor={isMemberOrCollaborator}
+            />
+          </Link>
+          <div
+            className={`hidden items-center space-x-6 font-title text-lg font-medium ${linkColor} md:flex`}
           >
-            <ul className="flex flex-col space-y-3 font-title text-3xl font-medium text-gray">
-              {[home, ...routes].map((route, i) => (
-                <motion.li
-                  key={route.path}
-                  initial={{ scale: 0.9, opacity: 0 }}
-                  animate={{ scale: 1, opacity: 1 }}
-                  transition={{ delay: i * 0.1 }}
-                >
-                  <Link
-                    href={route.path}
-                    className={`${
-                      isCurrent(route.path) ? "text-black" : ""
-                    } transition-colors hover:text-black`}
-                    onClick={() => setIsOpen(false)}
+            {routes.map((route) => (
+              <Link
+                key={route.path}
+                href={route.path}
+                className={`${
+                  isCurrent(route.path) ? `${currentLink}` : ""
+                } transition-colors ${isMemberOrCollaborator ? "hover:text-white" : "hover:text-black"}`}
+              >
+                {route.name}
+              </Link>
+            ))}
+          </div>
+          <motion.button
+            className={`${hamburgerMenuColor} material-symbols-outlined z-50 p-1 text-3xl md:hidden`}
+            onClick={() => setIsOpen(!isOpen)}
+            animate={{ rotate: isOpen ? 90 : 0 }}
+          >
+            {isOpen ? "close" : "menu"}
+          </motion.button>
+        </nav>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.nav
+              className="fixed bottom-0 left-0 right-0 top-0 z-40 flex flex-col justify-between bg-[#F0F0F0] px-7 pb-8 pt-20 shadow-lg"
+              exit={{ x: "100%", borderRadius: "50px" }}
+              initial={{ scale: 1.15 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.15 }}
+            >
+              <ul className="flex flex-col space-y-3 font-title text-3xl font-medium text-gray">
+                {[home, ...routes].map((route, i) => (
+                  <motion.li
+                    key={route.path}
+                    initial={{ scale: 0.9, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{ delay: i * 0.1 }}
                   >
-                    <h1>{route.name}</h1>
-                  </Link>
-                </motion.li>
-              ))}
-            </ul>
-            <div className="space-y-4">
-              <div className="space-y-2 font-medium text-gray">
-                <motion.p
-                  initial={{ scale: 0.9, opacity: 0 }}
-                  animate={{ scale: 1, opacity: 1 }}
-                  transition={{ delay: 0.1 }}
-                >
-                  {dict.social_media}
-                </motion.p>
-                <ul className="flex space-x-4">
-                  {dict.socials.map((social, i) => (
-                    <motion.li
-                      key={social.name}
-                      initial={{ scale: 0.9, opacity: 0 }}
-                      animate={{ scale: 1, opacity: 1 }}
-                      transition={{ delay: 0.3 + i * 0.1 }}
+                    <Link
+                      href={route.path}
+                      className={`${
+                        isCurrent(route.path) ? "text-black" : ""
+                      } transition-colors hover:text-black`}
+                      onClick={() => setIsOpen(false)}
                     >
-                      <Link
-                        href={social.url}
-                        target="_blank"
-                        rel="noopener noreferrer"
+                      <h1>{route.name}</h1>
+                    </Link>
+                  </motion.li>
+                ))}
+              </ul>
+              <div className="space-y-4">
+                <div className="space-y-2 font-medium text-gray">
+                  <motion.p
+                    initial={{ scale: 0.9, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{ delay: 0.1 }}
+                  >
+                    {dict.social_media}
+                  </motion.p>
+                  <ul className="flex space-x-4">
+                    {dict.socials.map((social, i) => (
+                      <motion.li
+                        key={social.name}
+                        initial={{ scale: 0.9, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        transition={{ delay: 0.3 + i * 0.1 }}
                       >
-                        <SocialIcon
-                          width={26}
-                          height={26}
-                          type={social.name.toLowerCase()}
-                          fill="#94959C"
-                        />
-                      </Link>
-                    </motion.li>
-                  ))}
-                </ul>
+                        <Link
+                          href={social.url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <SocialIcon
+                            width={26}
+                            height={26}
+                            type={social.name.toLowerCase()}
+                            fill="#94959C"
+                          />
+                        </Link>
+                      </motion.li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="flex w-full justify-end">
+                  <Image
+                    src="/logo/cesium.svg"
+                    width={30}
+                    height={34}
+                    alt="CeSIUM Logo Icon"
+                  />
+                </div>
               </div>
-              <div className="flex w-full justify-end">
-                <Image
-                  src="/logo/cesium.svg"
-                  width={30}
-                  height={34}
-                  alt="CeSIUM Logo Icon"
-                />
-              </div>
-            </div>
-          </motion.nav>
-        )}
-      </AnimatePresence>
+            </motion.nav>
+          )}
+        </AnimatePresence>
+      </div>
     </div>
   );
 };
