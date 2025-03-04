95b071343e1748a85465df87353593badcd89bd3
A112178 <a112178@alunos.uminho.pt>
2025-03-04 16:58:59 +0000

    refactor: move departments list to component

--- src/app/[lang]/about/departments/page.tsx
+++ src/app/[lang]/about/departments/page.tsx
@@ -1,71 +1,14 @@
 "use client";
 
-import DepartmentCard from "@/components/department-card";
+import DepartmentsList from "@/components/departments-list";
 import AppLink from "@/components/link";
 import PromotionalCard from "@/components/promotional-card";
 import { useDictionary } from "@/contexts/dictionary-provider";
 import { horizontalPadding, verticalPadding } from "@/lib/styling";
-import { CardType, type TeamData } from "@/lib/types";
-import { fetchTeamData } from "@/lib/utils";
-import { useState, useEffect } from "react";
+import { CardType } from "@/lib/types";
 
 export default function Departments() {
   const dict = useDictionary();
-
-  const currentYearRange = "2024-2025";
-  const [teamData, setTeamData] = useState<TeamData>([]);
-
-  useEffect(() => {
-    const aux = async () => {
-      const data: TeamData = await fetchTeamData(currentYearRange);
-      setTeamData(data);
-    };
-    void aux();
-  }, [currentYearRange]);
-
-  const gradient = (type: string) => {
-    switch (type) {
-      case "caos":
-        return ["[#0085FF]/5", "[#00D1FF]/5"];
-      case "dmc":
-        return ["[#FF00F5]/5", "[#FF2E00]/5"];
-      case "drem":
-        return ["[#0500FF]/5", "[#A500DE]/5"];
-      case "ped":
-        return ["[#E4B12E]/20", "[#ED7950]/20"];
-      case "rec":
-        return ["[#03A300]/5", "[#82E700]/5"];
-      default:
-        return ["[#0085FF]/5", "[#00D1FF]/5"];
-    }
-  };
-  // Short names, !! as they appear in dictionary !!
-  const shortName = (departmentName: string) => {
-    switch (departmentName) {
-      case "Centro de Apoio ao Open Source":
-        return "caos";
-      case "Departamento de Marketing e Conteúdo":
-        return "dmc";
-      case "Departamento de Relações Externas e Merch":
-        return "drem";
-      case "Departamento Pedagógico":
-        return "ped";
-      case "Departamento Recreativo":
-        return "rec";
-      default:
-        return "caos";
-    }
-  };
-
-  // List of department names, !! as they appear in team data !!
-  const departmentNames = [
-    "Centro de Apoio ao Open Source",
-    "Departamento de Marketing e Conteúdo",
-    "Departamento de Relações Externas e Merch",
-    "Departamento Pedagógico",
-    "Departamento Recreativo",
-  ];
-
   return (
     <main
       className={`flex flex-col gap-8 sm:gap-12 ${horizontalPadding + verticalPadding}`}
@@ -77,20 +20,10 @@ export default function Departments() {
         </h1>
         <p>{dict.about.departments.description}</p>
       </div>
-      <div className="grid grid-cols-1 gap-4 sm:gap-5 md:grid-cols-2">
-        {departmentNames.map((departmentName) => (
-          <DepartmentCard
-            key={departmentName}
-            name={departmentName}
-            shortName={shortName(departmentName)}
-            gradientFrom={gradient(shortName(departmentName))[0] ?? ""}
-            gradientTo={gradient(shortName(departmentName))[1] ?? ""}
-            hideTeam={false}
-            teamData={teamData}
-            yearRange={currentYearRange}
-          />
-        ))}
-      </div>
+      <DepartmentsList
+        hideTeam={false}
+        className="grid grid-cols-1 gap-4 sm:gap-5 md:grid-cols-2"
+      />
       <div className="px-2 md:px-5">
         <PromotionalCard type={CardType.Collaborate} />
       </div>
