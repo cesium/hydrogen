4a2bf1fb29db526be05cc769ee1ac54772c04f18
Diogo Matos <me@diogogmatos.dev>
2025-03-04 11:17:27 +0000

    chore: polishing corners

--- src/components/info-card.tsx
+++ src/components/info-card.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 interface InfoCardProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const InfoCard: React.FC<InfoCardProps> = ({ children }) => {
-    return (
-        <div className='bg-white rounded-2xl border border-black/10 min-w-[300px] overflow-hidden'>
-            {children}
-        </div>
-    );
+  return (
+    <div className="min-w-[300px] overflow-hidden rounded-2xl border border-black/10 bg-white">
+      {children}
+    </div>
+  );
 };
 
 export default InfoCard;
