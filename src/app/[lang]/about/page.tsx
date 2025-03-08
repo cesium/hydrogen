4740d22c047ba7d2b97b9d03d439d40863e018bb
Rui Faria <107812004+rfaria107@users.noreply.github.com>
2025-03-08 19:49:21 +0000

    feat:  `/about` page explore section (#113)
    * feat:  page explore section
    
    * fix: merge issue
    
    * feat:  page explore section
    
    * fix: merge issue
    
    * refactor to use a map
    
    * feat: /about page explore section
    
    * Apply suggestions from code review
    
    Co-authored-by: Diogo Matos <diogomatosemail@gmail.com>
    
    * dictionary fixes
    
    * chore: final things
    
    ---------
    
    Co-authored-by: Diogo Matos <diogomatosemail@gmail.com>
    Co-authored-by: Diogo Matos <me@diogogmatos.dev>

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,22 +1,30 @@
 "use client";
 
+import AboutSection from "@/components/about-section";
+import AboutSectionLayout from "@/components/about-section-layout";
+import AppLink from "@/components/link";
+import ProjectCard from "@/components/project-card";
 import { useDictionary } from "@/contexts/dictionary-provider";
 import Carousel from "@/components/carousel";
 import Image from "next/image";
 import { horizontalPadding } from "@/lib/styling";
 
 export default function About() {
-
   const dict = useDictionary();
   const dictAbout = dict.about;
-
   const images = dictAbout.sections.cesium.images;
-  
   return (
     <main>
-      <section className={`flex flex-col items-center text-center py-12 gap-4 sm:gap-6 border-b border-black/10 bg-black/5`}>
-        <p className="font-title font-medium text-2xl sm:text-3xl">{dictAbout.sections.cesium.title}</p>
-        <p className={horizontalPadding}>{dictAbout.sections.cesium.subtitle}</p>
+      {/* "What is CeSIUM?" */}
+      <section
+        className={`flex flex-col items-center gap-4 border-b border-black/10 bg-black/5 py-12 text-center sm:gap-6`}
+      >
+        <p className="font-title text-2xl font-medium sm:text-3xl">
+          {dictAbout.sections.cesium.title}
+        </p>
+        <p className={horizontalPadding}>
+          {dictAbout.sections.cesium.subtitle}
+        </p>
         <div className="w-full overflow-hidden pt-4 sm:pt-6">
           <Carousel
             autoplay={2000}
@@ -36,8 +44,57 @@ export default function About() {
             ))}
           />
         </div>
-        <p className={horizontalPadding}>{dictAbout.sections.cesium.description}</p>
+        <p className={horizontalPadding}>
+          {dictAbout.sections.cesium.description}
+        </p>
       </section>
+      {/* Projects */}
+      <AboutSectionLayout
+        linkName="see_more"
+        titleOrientation="vertical"
+        title={dict.about.projects.title}
+        subtitle={dict.about.projects.description}
+        href="/projects"
+        overflows
+      >
+        <div
+          className="flex flex-col md:flex-row md:gap-4"
+          style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+        >
+          <ProjectCard type="sei" />
+          <ProjectCard type="bugsbyte" />
+          <ProjectCard type="coderdojo" />
+        </div>
+      </AboutSectionLayout>
+      {/* Explore Further */}
+      <AboutSection dark>
+        <div className="flex flex-col gap-5 py-10 md:py-16">
+          <h2 className="w-full font-title text-2xl font-medium md:text-3xl">
+            {dict.about.explore.title}
+          </h2>
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-5">
+            {dict.about.explore.items.map((item) => (
+              <div key={item.title} className="flex flex-col gap-2 sm:gap-3">
+                <h3 className="font-bold">{item.title}</h3>
+                <p>{item.description}</p>
+                {item.links.map((link, index) => {
+                  const linkColor = "color" in link ? link.color : "primary";
+                  return (
+                    <div key={index}>
+                      <AppLink
+                        title={link.title}
+                        href={link.href}
+                        arrow={link.arrow as "back" | "forward" | "outward"}
+                        color={linkColor as "primary" | "blue"}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            ))}
+          </div>
+        </div>
+      </AboutSection>
     </main>
   );
-};
+}
