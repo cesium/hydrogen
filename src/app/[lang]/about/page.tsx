a68117ef8a2aef6f87555bab9a9274d282c08fdc
Gonçalo Fernandes <149530330+gxnca@users.noreply.github.com>
2025-03-08 18:22:33 +0000

    feat: /about page "O que é o CeSIUM?" section (#110)

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -1,29 +1,43 @@
 "use client";
-import AboutSectionLayout from "@/components/about-section-layout";
-import ProjectCard from "@/components/project-card";
+
 import { useDictionary } from "@/contexts/dictionary-provider";
+import Carousel from "@/components/carousel";
+import Image from "next/image";
+import { horizontalPadding } from "@/lib/styling";
 
 export default function About() {
+
   const dict = useDictionary();
+  const dictAbout = dict.about;
+
+  const images = dictAbout.sections.cesium.images;
+  
   return (
     <main>
-      <AboutSectionLayout
-        linkName="see_more"
-        titleOrientation="vertical"
-        title={dict.about.projects.title}
-        subtitle={dict.about.projects.description}
-        href="/projects"
-        overflows
-      >
-        <div
-          className="flex flex-col md:flex-row md:gap-4"
-          style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
-        >
-          <ProjectCard type="sei" />
-          <ProjectCard type="bugsbyte" />
-          <ProjectCard type="coderdojo" />
+      <section className={`flex flex-col items-center text-center py-12 gap-4 sm:gap-6 border-b border-black/10 bg-black/5`}>
+        <p className="font-title font-medium text-2xl sm:text-3xl">{dictAbout.sections.cesium.title}</p>
+        <p className={horizontalPadding}>{dictAbout.sections.cesium.subtitle}</p>
+        <div className="w-full overflow-hidden pt-4 sm:pt-6">
+          <Carousel
+            autoplay={2000}
+            pagination
+            overflow
+            loop
+            items={images.map((image, index) => (
+              <div key={index} className="flex justify-center">
+                <Image
+                  src={image.src}
+                  width={490}
+                  height={367}
+                  alt={image.alt}
+                  className="h-[237px] w-full rounded-xl object-cover sm:h-[367px] sm:max-w-full"
+                />
+              </div>
+            ))}
+          />
         </div>
-      </AboutSectionLayout>
+        <p className={horizontalPadding}>{dictAbout.sections.cesium.description}</p>
+      </section>
     </main>
   );
-}
+};
