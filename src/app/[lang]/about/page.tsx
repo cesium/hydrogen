de1233e9b74e416018ad4e4f0889346a5c5b5ed3
11GG20 <fl11gg20@gmail.com>
2025-03-11 23:56:44 +0000

    fix: improve hero section

--- src/app/[lang]/about/page.tsx
+++ src/app/[lang]/about/page.tsx
@@ -3,9 +3,6 @@
 import AboutSection from "@/components/about-section";
 import Carousel from "@/components/carousel";
 import { useDictionary } from "@/contexts/dictionary-provider";
-import Carousel from "@/components/carousel";
-import Image from "next/image";
-import { horizontalPadding } from "@/lib/styling";
 
 export default function About() {
 
@@ -14,12 +11,12 @@ export default function About() {
   const heroItems = [
     <div
       key="title"
-      className="flex md:flex-1 items-center"
+      className="flex lg:flex-1 items-center"
     >
-      <p className="bg-gradient-to-r from-black/50 via-black/25 to-black/50 bg-clip-text font-title font-medium leading-[125%] text-transparent text-[36px] sm:text-[48px] md:text-[38px] xl:text-[48px] w-[343px] sm:w-[430px] lg:w-[460px] h-fit xl:w-[565px]">
+      <p className="bg-gradient-to-r from-black/50 via-black/25 to-black/50 bg-clip-text font-title font-medium leading-[125%] text-transparent text-[36px] sm:text-[40px] xl:text-[48px] w-[343px] sm:w-[358px] lg:w-[460px] h-fit xl:w-[565px]">
         Centro de
         <br /> Estudantes de
-        <span className="font-title text-[36px] sm:text-[48px] md:text-[38px] xl:text-[48px] text-black">
+        <span className="font-title text-[36px] sm:text-[40px] xl:text-[48px] text-black">
           {" "}
           Engenharia Informática
         </span>{" "}
@@ -29,14 +26,14 @@ export default function About() {
 
     <div
       key="subtitle"
-      className="flex-col items-center"
+      className="flex flex-col justify-center sm:h-[300px]"
     >
       <div className="text-justify font-sans font-normal leading-[24px] text-[#6E6E6E]">
-        <p className="text-[12px] md:text-[13px] lg:text-[15px] xl:text-[16px] w-[343px] h-fit sm:w-[480px] xl:w-[636px]">
+        <p className="text-[15px] xl:text-[16px] w-[343px] h-fit sm:w-[480px] md:w-[636px] lg:w-[480px] xl:w-[636px]">
           {dict.about.sections.hero.description}
         </p>
       </div>
-      <div className="mt-3 lg:mt-5 text-right font-sans text-[12px] md:text-[13px] font-normal text-black lg:text-[15px] xl:text-[16px]">
+      <div className="mt-3 lg:mt-5 text-right font-sans text-[15px] font-normal text-black xl:text-[16px]">
         <p>Pedro Rangel Henriques</p>
         <p>{dict.about.sections.hero.subtitle}</p>
       </div>
@@ -46,23 +43,30 @@ export default function About() {
   return (
     <main className="flex-col items-center justify-center snap-y snap-mandatory overflow-y-scroll h-screen">
       <AboutSection>
-        <section
-          className={`w-full h-fit items-center justify-center pb-64 pt-44 snap-always snap-center`}
-        >
-          <div className="hidden h-fit items-center justify-center md:flex">
-            {heroItems.map((item, index) => (item))}
-          </div>
+          <section
+            className={`relative w-full h-fit items-center justify-center pb-64 pt-44 snap-always snap-start`}
+          >
+            <div className="hidden h-fit items-center justify-center lg:flex">
+              {heroItems.map((item, index) => (item))}
+            </div>
 
-          <div className="block md:hidden">
-            <Carousel
-              autoplay={2000}
-              pagination
-              items={heroItems.map((item, index) => (
-                <div key={index} className="flex justify-center items-center">{item}</div>
-              ))}
-            />
+            <div className="block lg:hidden">
+              <Carousel
+                autoplay={2000}
+                pagination
+                items={heroItems.map((item, index) => (
+                  <div key={index} className="flex justify-center items-center">{item}</div>
+                ))}
+              />
+            </div>
+          
+          <div className="absolute bottom-[30px] left-0 right-0 flex flex-col items-center justify-center gap-1 h-[56px]">
+                <p>Desliza para ver mais</p>
+                <span className="material-symbols-outlined">arrow_downward</span>
           </div>
-        </section>
+          
+          </section>
+
       </AboutSection>
 
       <section className="snap-always snap-center h-screen">
