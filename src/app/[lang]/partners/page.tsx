c2ed7f8e3b66701250cdfec460e28c58bed9c816
fp <a107009@alunos.uminho.pt>
2025-03-13 18:37:50 +0000

    partner page

--- src/app/[lang]/partners/page.tsx
+++ src/app/[lang]/partners/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { horizontalPadding, verticalPadding } from "@/lib/styling";
 import PartnerCard from "@/components/partner-card";
+import PromotionalCard from "@/components/promotional-card";
 import { useDictionary } from "@/contexts/dictionary-provider";
+import { CardType} from "@/lib/types";
 
 export default function Partners() {
   const dict = useDictionary();
@@ -9,11 +11,17 @@ export default function Partners() {
   const partners = dict.partners;
 
   return (
-    <main
-      className={`flex flex-col gap-8 sm:gap-12 ${horizontalPadding} ${verticalPadding}`}
-    >
-      <div className="grid grid-cols-1 gap-2.5 sm:gap-3 md:grid-cols-2 2xl:grid-cols-3">
-        {partners.map((partner, index) => (
+    <main className={`flex flex-col gap-8 sm:gap-12`}>
+      <div className={`flex flex-col gap-3.5 max-w-7xl ${horizontalPadding} py-40`}>
+        <h1 className={`font-title text-3xl sm:text-5xl font-medium`}>
+          {dict.partners.title}
+        </h1>
+        <p>
+        {dict.partners.description}
+        </p>
+      </div>
+      <div className={`grid grid-cols-1 gap-2.5 sm:gap-3 md:grid-cols-2 2xl:grid-cols-3 ${horizontalPadding} pt-14 bg-muted border-t border-zinc-200`}>
+        {partners.list.map((partner, index) => (
           <PartnerCard
             key={index}
             title={partner.title}
@@ -23,6 +31,11 @@ export default function Partners() {
             perks={partner.perks}
           />
         ))}
+      </div>
+      <div className={`${horizontalPadding} pb-16`}>
+        <PromotionalCard
+            type={CardType.Membership}
+        />
       </div>
     </main>
   );
