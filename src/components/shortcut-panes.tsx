7689d1cfe0b07c5cadd761521d1d53c2c3cb3a26
João Lobo <joao@lobo.is>
2025-03-22 16:28:08 +0000

    feat: improve mobile panes

--- src/components/shortcut-panes.tsx
+++ src/components/shortcut-panes.tsx
@@ -13,21 +13,25 @@ interface ShortcutPanesProps {
 }
 
 const ShortcutPanes = ({ shortcuts }: ShortcutPanesProps) => (
-  <div className="flex flex-col gap-8 rounded-2xl border border-dark/10 bg-white py-12 xl:flex-row xl:px-11 xl:py-0">
+  <div className="flex flex-col gap-0 rounded-2xl border border-dark/10 bg-white pt-8 xl:flex-row xl:gap-8 xl:px-11 xl:pb-0 xl:pt-0">
     {shortcuts.map((shortcut, index) => (
       <Link
         key={index}
         href={shortcut.href}
-        className="group relative w-full bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-black/10 from-20% to-transparent to-60% px-14 py-9 xl:bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))]"
+        className={`group relative w-full bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-stroke from-20% to-transparent to-60% px-6 py-9 xl:bg-[radial-gradient(ellipse_at_bottom_left,_var(--tw-gradient-stops))] xl:px-14 ${index == shortcuts.length - 1 ? "rounded-b-xl xl:rounded-none" : ""}`}
       >
-        <div className="clip-triangle absolute bottom-0 left-0 h-8 w-full bg-white xl:bottom-auto xl:top-0 xl:h-full xl:w-11"></div>
+        <div
+          className={`clip-triangle absolute left-0 h-8 w-full bg-white xl:bottom-auto xl:left-[-1px] xl:top-0 xl:h-full xl:w-11 ${index == shortcuts.length - 1 ? "bottom-0 rounded-2xl xl:rounded-none" : "bottom-[-1px]"}`}
+        ></div>
         <span className="flex flex-row items-center gap-2">
           <h1 className="font-title text-xl font-medium">{shortcut.title}</h1>
           <span className="material-symbols-outlined text-3xl text-dark/50 transition-transform group-hover:translate-x-1">
             arrow_forward
           </span>
         </span>
-        <p className="ml-0 text-base xl:ml-5">{shortcut.description}</p>
+        <p className="ml-0 pb-8 text-base xl:ml-5 xl:pb-0">
+          {shortcut.description}
+        </p>
       </Link>
     ))}
   </div>
