443b3e30b7d83727b6e1965fb106c73217966bce
Diogo Matos <diogomatosemail@gmail.com>
2025-03-24 19:05:55 +0000

    fix: carousel items not centered & improve about page hero (#168)

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -7,10 +7,7 @@ import { CardType } from "@/lib/types";
 import { horizontalPadding } from "@/lib/styling";
 import Image from "next/image";
 import { useDictionary } from "@/contexts/dictionary-provider";
-import {
-  relativeScrollTo,
-  useScrollState,
-} from "@/contexts/scrollstate-provider";
+import { scrollTo, useScrollState } from "@/contexts/scrollstate-provider";
 
 export default function Home() {
   const dict = useDictionary();
@@ -58,8 +55,9 @@ export default function Home() {
             </h2>
           </div>
         </div>
+        {/* See More */}
         <button
-          onClick={() => relativeScrollTo(50)}
+          onClick={() => scrollTo(window.innerHeight - 72)}
           className={`mb-8 flex h-14 flex-col items-center justify-center gap-1 text-white transition-opacity duration-300 ${isScrolledTop ? "opacity-100" : "opacity-0"}`}
         >
           <p>{dict.button.swipe}</p>
