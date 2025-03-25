31e8a0a7453341e18f4722f2c369240ff882f663
RicoPleasure <esprazj.dev@gmail.com>
2025-03-25 01:12:03 +0000

    fix: carousel bug

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -80,7 +80,10 @@ export default function Carousel({
         className="custom-swiper"
       >
         {items.map((item, index) => (
-          <SwiperSlide key={index} className={`flex justify-center`}>
+          <SwiperSlide
+            key={index}
+            className={`flex justify-center ${!overflow ? "!w-full" : ""}`}
+          >
             {item}
           </SwiperSlide>
         ))}
