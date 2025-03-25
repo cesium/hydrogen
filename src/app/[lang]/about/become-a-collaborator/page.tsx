4f3d0d880391d594e4b946917e4fd0655f72bc97
Frederico Afonso <137807927+Nellrin@users.noreply.github.com>
2025-03-25 15:38:35 +0000

    feat: `about/become-a-collaborator` hero section (#99)
    * feat: /about/become-a-collaborator Hero (#87)
    
    * feat: /about/become-a-collaborator Hero (#87)
    
    * feat: /about/become-a-collaborator Hero (#87)
    
    * feat: hero done
    
    * fix: remove unecessary imports
    
    ---------
    
    Co-authored-by: jojocoelho <jhcoelho03@gmail.com>

--- src/app/[lang]/about/become-a-collaborator/page.tsx
+++ src/app/[lang]/about/become-a-collaborator/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import Image from "next/image";
+import Button from "@/components/button";
 import CallSubscribe from "@/components/call-subscribe";
+import { verticalPadding } from "@/lib/styling";
 import { useDictionary } from "@/contexts/dictionary-provider";
 
 export default function BecomeACollaborator() {
@@ -8,7 +11,48 @@ export default function BecomeACollaborator() {
 
   return (
     <main>
-      Become a Collaborator
+      <section
+        className={`relative grid min-h-[380px] grid-flow-row items-start bg-blue text-white lg:grid-flow-col lg:items-start`}
+      >
+        <div
+          className={`max-w-1/2 flex flex-col gap-8 ${verticalPadding} px-5 sm:pr-0 md:px-16 lg:pl-28 2xl:pl-60`}
+        >
+          <div>
+            <h1 className="text-gradient font-title text-4xl font-medium text-white/50 lg:text-5xl">
+              {dict.about.become_a_collaborator.hero.title}{" "}
+              <span className="text-white">CeSIUM</span>
+            </h1>
+          </div>
+          <p className="relative z-10 text-lg">
+            {dict.about.become_a_collaborator.hero.description}
+          </p>
+          <Button
+            title={dict.about.become_a_collaborator.hero.button}
+            style="style1"
+            color="blue"
+            as="link"
+            href="https://cesium.link/f/recrutamento"
+          />
+        </div>
+        <div className="pointer-events-none flex h-full select-none items-end justify-center sm:hidden sm:justify-end xl:flex">
+          <Image
+            src="/vectors/collaborator.svg"
+            alt="Torna-te membro"
+            height={331}
+            width={528}
+            className="h-fit w-[323px] lg:w-[528px]"
+          />
+        </div>
+        <div className="pointer-events-none absolute bottom-0 right-0 hidden h-full select-none items-end justify-center sm:flex sm:justify-end xl:hidden">
+          <Image
+            src="/vectors/collaborator.svg"
+            alt="Torna-te colaborador"
+            height={331}
+            width={528}
+            className="h-fit w-[323px]"
+          />
+        </div>
+      </section>
       <div>
         <CallSubscribe
           title={dict.callsub.collaborators.title}
