704128badddbb49135f47217fa7b5377a541e3ca
João Coelho <116276331+JoaoCoelho2003@users.noreply.github.com>
2025-10-01 16:28:43 +0100

    fix: events listing (#205)
    * fix: events listing
    
    * fix: format
    
    * fix: events listing not working in certain use cases
    
    * feat: expand multiple day events to many one day events
    
    * fix: removed hardcoded events

--- src/components/event-list-card.tsx
+++ src/components/event-list-card.tsx
@@ -1,8 +1,15 @@
 "use client";
+
 import type { EventListProps, Event } from "../lib/types";
 import { EventCardCalendar } from "./event-card-calendar";
 import { useDictionary, useLang } from "@/contexts/dictionary-provider";
-import { isSameDay } from "../lib/utils";
+import {
+  isSameDay,
+  isWithinRange,
+  isToday,
+  isPastDay,
+  isFutureDay,
+} from "../lib/utils";
 import { fullLocale } from "@/lib/locale";
 
 export function EventListCard({
@@ -13,7 +20,6 @@ export function EventListCard({
 }: EventListProps) {
   const dict = useDictionary();
   const lang = useLang();
-  const currentDate = new Date();
 
   const filteredEvents = selectedDate
     ? events.filter((event) => {
@@ -21,21 +27,36 @@ export function EventListCard({
         const eventEnd = new Date(event.end);
         return (
           isSameDay(eventStart, selectedDate) ||
-          (eventEnd && eventStart <= selectedDate && eventEnd >= selectedDate)
+          (eventEnd && isWithinRange(selectedDate, eventStart, eventEnd))
         );
       })
     : events;
 
+  const todayEvents = filteredEvents.filter((event) => {
+    const eventStart = new Date(event.start);
+    const eventEnd = new Date(event.end);
+    const today = new Date();
+
+    return (
+      isToday(eventStart) ||
+      isToday(eventEnd) ||
+      isWithinRange(today, eventStart, eventEnd)
+    );
+  });
+
   const futureEvents = filteredEvents
-    .filter((event) => new Date(event.start) >= currentDate)
+    .filter((event) => {
+      const eventStart = new Date(event.start);
+      return isFutureDay(eventStart);
+    })
     .sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime());
 
   const pastEvents = filteredEvents
-    .filter((event) => new Date(event.end) < currentDate)
+    .filter((event) => {
+      const eventEnd = new Date(event.end);
+      return isPastDay(eventEnd);
+    })
     .sort((a, b) => new Date(b.end).getTime() - new Date(a.end).getTime());
-
-  const visibleFutureEvents = futureEvents.slice(0, 5);
-  const visiblePastEvents = pastEvents.slice(0, 5);
 
   const renderEventList = (
     eventList: Event[],
@@ -93,13 +114,11 @@ export function EventListCard({
           <span className="material-symbols-outlined ml-1 text-xl">close</span>
         </button>
       )}
+      {todayEvents.length > 0 &&
+        renderEventList(todayEvents, todayEvents, dict.events.todayEvents)}
       <div className="flex w-full flex-row items-center gap-6">
-        {renderEventList(
-          visibleFutureEvents,
-          futureEvents,
-          dict.events.futureEvents,
-        )}
-        {renderEventList(visiblePastEvents, pastEvents, dict.events.pastEvents)}
+        {renderEventList(futureEvents, futureEvents, dict.events.futureEvents)}
+        {renderEventList(pastEvents, pastEvents, dict.events.pastEvents)}
       </div>
       {!isLoading && filteredEvents.length === 0 && (
         <div className="text-center text-black/50">{dict.events.noEvents}</div>
