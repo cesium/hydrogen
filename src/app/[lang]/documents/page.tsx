87179a7cfd3316bc4d6a2ac6c98baf262f46f450
Martim <a107320@uminho.pt>
2025-12-11 21:20:04 +0000

    fix: formatting

--- src/app/[lang]/documents/page.tsx
+++ src/app/[lang]/documents/page.tsx
@@ -1,15 +1,14 @@
-"use client"
+"use client";
 import { useDictionary } from "@/contexts/dictionary-provider";
 
 export default function Documents() {
-      const dict = useDictionary();
-    
-    return (
-        <main>
-            <h1 className="font-title text-3xl font-mediumpx-2 md:px-5">
-                {dict.documents.title}
+  const dict = useDictionary();
 
-            </h1>
-        </main>
-    )
+  return (
+    <main>
+      <h1 className="font-mediumpx-2 font-title text-3xl md:px-5">
+        {dict.documents.title}
+      </h1>
+    </main>
+  );
 }
